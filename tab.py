569d6d9576b007fd04dc4c8c0f36f7f1779103f5
Zach Dillon <zdillon90@gmail.com>
2022-05-25 11:47:59 +0000

    moving back to csv

--- tab.py
+++ tab.py
@@ -1,6 +1,3 @@
 import tabula
-from tabulate import tabulate
 
-data = tabula.convert_into("example.pdf", "output.csv", output_format="csv", pages='all')
-
-print(data)
+tabula.convert_into("example.pdf", "output.csv", output_format="csv", pages='all')
